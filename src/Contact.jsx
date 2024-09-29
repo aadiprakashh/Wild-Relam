d0cd1394f237bb8d5f9b95a1c38092c8a988555d
Aadi Prakash <aadiaju4319@gmail.com>
2024-09-29 15:18:43 +0530

    Initial commit

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,9 +3,9 @@ import "./contact.css";
 
 export default function Contact() {
   return (
-    <div>
+    <div className="contact">
       <section className="contact-section"></section>
-      <div className="container-fluid d-flex align-items-center justify-content-evenly p-5">
+      {/* <div className="container-fluid d-flex align-items-center justify-content-evenly p-5">
         <div className="col-md-5 contact">
           <h1>Contact Us</h1>
           <p>
@@ -84,6 +84,54 @@ export default function Contact() {
             </div>
           </div>
         </div>
+      </div> */}
+      <div className="contact-header text-center pt-5">
+        <h1>Contact Us</h1>
+        <p>
+          If you have any questions or need further information about Indian
+          wildlife, feel free to reach out to us. We are here to help!
+        </p>
+      </div>
+      <div class="container-fluid d-flex justify-content-evenly p-5">
+        <section class="contact-form col-md-5 p-5">
+          <h2>Get in Touch</h2>
+          <form action="https://api.web3forms.com/submit" method="POST">
+            <input
+              type="hidden"
+              name="access_key"
+              value="56362fab-8287-4be2-8606-4ead555cc964"
+            />
+            <div class="form-group">
+              <label for="name">Name:</label>
+              <input type="text" id="name" name="name" required />
+            </div>
+            <div class="form-group">
+              <label for="email">Email:</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+            <div class="form-group">
+              <label for="message">Message:</label>
+              <textarea id="message" name="message" required></textarea>
+            </div>
+            <button type="submit">Send Message</button>
+          </form>
+        </section>
+
+        <section class="address-section col-md-5 p-5">
+          <h2>Our Address</h2>
+          <address>
+            123 Web Dev Lane
+            <br />
+            Coding City, TX 12345
+            <br />
+            United States
+            <br />
+            <br />
+            Phone: (123) 456-7890
+            <br />
+            Email: info@example.com
+          </address>
+        </section>
       </div>
     </div>
   );
